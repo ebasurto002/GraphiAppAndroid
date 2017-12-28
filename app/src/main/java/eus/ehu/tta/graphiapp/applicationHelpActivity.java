696df14517bcea5413ca47159516ca07b32d91ee
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-12-28 19:35:06 +0100

    Añadido panel a las actividades de home del usuario y ayuda

--- app/src/main/java/eus/ehu/tta/graphiapp/applicationHelpActivity.java
+++ app/src/main/java/eus/ehu/tta/graphiapp/applicationHelpActivity.java
@@ -1,16 +1,21 @@
 package eus.ehu.tta.graphiapp;
 
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.VideoView;
 
-public class applicationHelpActivity extends AppCompatActivity {
+public class applicationHelpActivity extends drawerBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_application_help);
+        ViewGroup viewGroup = findViewById(R.id.content_frame);
+        LayoutInflater inflater = LayoutInflater.from(applicationHelpActivity.this);
+        View inflatedView = inflater.inflate(R.layout.activity_application_help,viewGroup,false);
+        viewGroup.addView(inflatedView);
 
         VideoView videoView = (VideoView)findViewById(R.id.helpVideoView);
         videoView.setVideoURI(Uri.parse("https://dl.dropboxusercontent.com/s/e94hhon3f6dr6tb/Vídeo%20de%20ayuda%20de%20GraphiApp.mp4?dl=0"));
--- app/src/main/java/eus/ehu/tta/graphiapp/userHomeActivity.java
+++ app/src/main/java/eus/ehu/tta/graphiapp/userHomeActivity.java
@@ -2,12 +2,18 @@ package eus.ehu.tta.graphiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class userHomeActivity extends AppCompatActivity {
+public class userHomeActivity extends drawerBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_home);
+        ViewGroup viewGroup = findViewById(R.id.content_frame);
+        LayoutInflater inflater = LayoutInflater.from(userHomeActivity.this);
+        View inflatedView = inflater.inflate(R.layout.activity_user_home,viewGroup,false);
+        viewGroup.addView(inflatedView);
     }
 }
