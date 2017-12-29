f5eeab5e48ce4aa87248c3974adda672c6713ea3
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-12-29 14:01:14 +0100

    Simplificado código de onCreate de las actividades de home del usuario y ayuda

--- app/src/main/java/eus/ehu/tta/graphiapp/applicationHelpActivity.java
+++ app/src/main/java/eus/ehu/tta/graphiapp/applicationHelpActivity.java
@@ -5,7 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.VideoView;
 
 public class applicationHelpActivity extends drawerBaseActivity {
@@ -13,12 +13,11 @@ public class applicationHelpActivity extends drawerBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ViewGroup viewGroup = findViewById(R.id.content_frame);
+        FrameLayout frameLayout = findViewById(R.id.content_frame);
         LayoutInflater inflater = LayoutInflater.from(applicationHelpActivity.this);
-        View inflatedView = inflater.inflate(R.layout.activity_application_help,viewGroup,false);
-        viewGroup.addView(inflatedView);
+        inflater.inflate(R.layout.activity_application_help,frameLayout,true);
 
-        VideoView videoView = (VideoView)findViewById(R.id.helpVideoView);
+        VideoView videoView = findViewById(R.id.helpVideoView);
         videoView.setVideoURI(Uri.parse("https://dl.dropboxusercontent.com/s/e94hhon3f6dr6tb/Vídeo%20de%20ayuda%20de%20GraphiApp.mp4?dl=0"));
         videoView.start();
     }
--- app/src/main/java/eus/ehu/tta/graphiapp/userHomeActivity.java
+++ app/src/main/java/eus/ehu/tta/graphiapp/userHomeActivity.java
@@ -1,21 +1,19 @@
 package eus.ehu.tta.graphiapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 public class userHomeActivity extends drawerBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ViewGroup viewGroup = findViewById(R.id.content_frame);
+        FrameLayout frameLayout = findViewById(R.id.content_frame);
         LayoutInflater inflater = LayoutInflater.from(userHomeActivity.this);
-        View inflatedView = inflater.inflate(R.layout.activity_user_home,viewGroup,false);
-        viewGroup.addView(inflatedView);
+        inflater.inflate(R.layout.activity_user_home,frameLayout,true);
     }
 
     public void goToHelp(View view) {
