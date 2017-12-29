3ef13333c50e3e25866837dcc95792cc760a937a
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-12-29 17:33:15 +0100

    Añadida actividad de niveles del modo individual

--- app/src/main/java/eus/ehu/tta/graphiapp/LevelsActivity.java
+++ app/src/main/java/eus/ehu/tta/graphiapp/LevelsActivity.java
@@ -6,23 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 
-public class IndividualModeActivity extends drawerStudentActivity {
+public class LevelsActivity extends drawerStudentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FrameLayout frameLayout = findViewById(R.id.content_frame);
         LayoutInflater inflater = LayoutInflater.from(this);
-        inflater.inflate(R.layout.activity_individual_mode,frameLayout,true);
+        inflater.inflate(R.layout.activity_levels,frameLayout,true);
     }
 
     public void goBack(View view) {
-        Intent intent = new Intent(this,GameModesActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToLevels(View view) {
-        Intent intent = new Intent(this,LevelsActivity.class);
+        Intent intent = new Intent(this,IndividualModeActivity.class);
         startActivity(intent);
     }
 }
