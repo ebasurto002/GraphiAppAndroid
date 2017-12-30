6b0c46feebf760f6cc67a46a53f7c28efac59b75
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-12-30 18:49:37 +0100

    Añadidos controles para el vídeo de ayuda

--- app/src/main/java/eus/ehu/tta/graphiapp/applicationHelpActivity.java
+++ app/src/main/java/eus/ehu/tta/graphiapp/applicationHelpActivity.java
@@ -3,9 +3,11 @@ package eus.ehu.tta.graphiapp;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class applicationHelpActivity extends drawerStudentActivity {
@@ -19,7 +21,24 @@ public class applicationHelpActivity extends drawerStudentActivity {
 
         VideoView videoView = findViewById(R.id.helpVideoView);
         videoView.setVideoURI(Uri.parse("https://dl.dropboxusercontent.com/s/e94hhon3f6dr6tb/Vídeo%20de%20ayuda%20de%20GraphiApp.mp4?dl=0"));
-        videoView.start();
+        MediaController controller = new MediaController(this)
+        {
+            @Override
+            public void hide() {
+            }
+
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent event)
+            {
+                if (event.getKeyCode() == KeyEvent.KEYCODE_BACK)
+                {
+                    finish();
+                }
+                return super.dispatchKeyEvent(event);
+            }
+        };
+        controller.setAnchorView(videoView);
+        videoView.setMediaController(controller);
     }
 
     public void goBack(View view) {
