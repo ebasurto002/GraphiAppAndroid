22dd11acbd9ebd35b802def064a1968fd16ab622
Estrella Basurto <ebasurto002@ehu.es>
2018-01-19 18:01:29 +0100

    Teacher home layout en marcha

--- app/src/main/java/eus/ehu/tta/graphiapp/TeacherHomeActivity.java
+++ app/src/main/java/eus/ehu/tta/graphiapp/TeacherHomeActivity.java
@@ -2,14 +2,18 @@ package eus.ehu.tta.graphiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
 
-public class TeacherHomeActivity extends AppCompatActivity {
+public class TeacherHomeActivity extends drawerTeacherActivity {
 
     public static final String EXTRA_LOGIN = "login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_home);
+        FrameLayout frameLayout = findViewById(R.id.content_frame);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        inflater.inflate(R.layout.activity_teacher_home,frameLayout,true);
     }
 }
